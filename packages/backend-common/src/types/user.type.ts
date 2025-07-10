bc0a91ac474ba035ba995faecbd6200caec3f17e
Shoyeb45 <shoyebff45@gmail.com>
2025-07-10 18:47:41 +0530

    Initialised db package and backend-common package for code sharing, also initialised http-server and websocker and added signup, signin, create-room endpoint

---
+++ packages/backend-common/src/types/user.type.ts
@@ -0,0 +1,30 @@
+import  { z } from "zod";
+
+export const userSignupSchema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(5).max(20)
+        .refine((password) => {
+            return /[A-Z]/.test(password);
+        }, {
+            message: "Password must include at least one uppercase letter"
+        })
+        .refine((password) => /[a-z]/.test(password), {
+            message: "Password must include at least one lowercase letter"
+        })
+        .refine((password) => /\d/.test(password), {
+            message: "Password must include at least one number"
+        })
+        .refine((password) => /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(password), {
+            message: "Password must include at least one special character"
+        })
+});
+
+
+export const userSiginSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+});
+
+export type userSigninType = z.infer<typeof userSiginSchema>;
+export type userSignupType =  z.infer<typeof userSignupSchema>;
