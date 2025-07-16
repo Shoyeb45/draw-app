70b9b5790fea0b05ed6eeb59a795c95b262c8485
Shoyeb45 <shoyebff45@gmail.com>
2025-07-16 10:42:28 +0530

    Refactor WebSocket connection handling to include user verification and cookie parsing

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,18 @@
 import { createWebSocket } from "./utils/webSocket";
-
+import * as cookie from "cookie";
+import { verifyUser } from "./utils/verifyUser";
 const wss = createWebSocket();
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, request) => {
+    const cookies = cookie.parse(request.headers.cookie || "");
+    const token = cookies["token"];
+
+    const userId = verifyUser(token);
+
+    if (!userId) {
+        console.log("Failed to verify user")
+        return null;
+    }
     ws.on('message', (data, isBinary) => {
         ws.send("Pong")
     })
--- apps/ws-backend/src/utils/verifyUser.ts
+++ apps/ws-backend/src/utils/verifyUser.ts
@@ -1,3 +1,28 @@
-export const verifyUser = (token: string) =>  {
+import jwt from "jsonwebtoken";
+import { config } from "@repo/backend-common";
+import prisma  from "@repo/db/client";
+
+export function verifyUser(token: string | undefined): string | null  {
     // verify the token with jwt secret and return true or false 
+    try {
+        if (!token) {
+            console.log("Given token is null");
+            return null;
+        }
+
+        const decoded = jwt.verify(token, config.jwt_secret ?? "JWT_SECRET");
+
+        if (typeof decoded == "string" || !decoded || !decoded.id) {
+            console.log("Decoded is a type of string or user id is not present")
+            return null;
+        }
+
+        return decoded.id;
+    } catch(e) {
+        if (e instanceof Error) {
+            console.log(e.message)
+        } 
+
+        return null;
+    }
 }
